e9fd4c9074775b3f0af54b91716ab27726d1d22a
marcoCC33 <marcuso5454@outlook.com>
2026-05-19 14:30:12 -0600

    Error tipografico
    #5 intall -> install

--- instalar_allegro.sh
+++ instalar_allegro.sh
@@ -2,6 +2,6 @@
 echo "Instalando dependecias de Allegro 5..."
 
 sudo apt install -y liballegro5-dev liballegro-image5-dev liballegro-font5-dev liballegro-primitives5-dev
-sudo apt intall -y libpcap-dev
+sudo apt install -y libpcap-dev
 
 echo "Instalación completada, ahora es posible ejecutar 'make' para compilar el sniffer."
