d979246620d373f6048b6c850bca2dee19d0b9e1
marcoCC33 <marcuso5454@outlook.com>
2026-05-20 14:13:12 -0600

    Lectura de 5 paquetes e implementación de allegro
    Se implementa el código de Juan para leer 5 paquetes más la implentación de Allegro5 en packet sniffer

--- packetSniffer.cpp
+++ packetSniffer.cpp
@@ -1,38 +1,89 @@
-#include <iostream>
+// Librerías de Allegro
 #include <allegro5/allegro.h>
 
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_primitives.h>
+// Librerías de Libpcap
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <pcap/pcap.h>
 
-using namespace std;
+// Librerías de C/C++
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    //Inicializar Allegro 5
-	if (!al_init())						return -1;
-	if (!al_init_image_addon())			return -1;
-	if (!al_init_primitives_addon())	return -1;
-	if (!al_install_keyboard())			return -1;
-	if (!al_install_mouse())			return -1;
-    
-	ALLEGRO_EVENT_QUEUE* cola_eventos = al_create_event_queue();
-	ALLEGRO_EVENT_QUEUE* cola_pantalla = al_create_event_queue();
-	ALLEGRO_TIMER* tiempo = al_create_timer(1.0 / 60);
-	ALLEGRO_EVENT eventos;
-	Pantalla pant(null, "Packet Sniffer");
-    bool programa_corriendo = true, bucle = true;
-    
-	al_register_event_source(cola_eventos, al_get_keyboard_event_source());
-	al_register_event_source(cola_eventos, al_get_mouse_event_source());
-	al_register_event_source(cola_eventos, al_get_timer_event_source(tiempo));
-	al_register_event_source(cola_pantalla, al_get_display_event_source((ALLEGRO_DISPLAY*)pant.get_display()));
+int link_hdr_length = 0;
 
-    //Lógica del programa
-    while (programa_corriendo) {
-        printf("Sniffeando\n");
-        programa_corriendo = false;
-    }
+void call_me(u_char *user, const struct pcap_pkthdr *pkthdr,
+             const u_char *packetd_ptr) {
+  packetd_ptr += link_hdr_length;
+  struct ip *ip_hdr = (struct ip *)packetd_ptr;
+ 
+  char packet_srcip[INET_ADDRSTRLEN];             
+  char packet_dstip[INET_ADDRSTRLEN];             
+  strcpy(packet_srcip, inet_ntoa(ip_hdr->ip_src));
+  strcpy(packet_dstip, inet_ntoa(ip_hdr->ip_dst));
+  int packet_id = ntohs(ip_hdr->ip_id),           
+      packet_ttl = ip_hdr->ip_ttl,                
+      packet_tos = ip_hdr->ip_tos,                
+      packet_len = ntohs(ip_hdr->ip_len),         
+      packet_hlen = ip_hdr->ip_hl;                
 
-    return 0;
+  printf("************************************"
+         "**************************************\n");
+  printf("ID: %d | SRC: %s | DST: %s | TOS: 0x%x | TTL: %d\n", packet_id,
+         packet_srcip, packet_dstip, packet_tos, packet_ttl);
+}
+
+int main(int argc, char const *argv[]) {
+  // Inicialización de Allegro
+  if (!al_init()) {
+    printf("ERR: No se pudo inicializar Allegro\n");
+    return -1;
+  }
+
+  // Creación de la ventana (display)
+  ALLEGRO_DISPLAY *display = al_create_display(640, 480);
+  if (!display) {
+    printf("ERR: No se pudo crear el display de Allegro\n");
+    return -1;
+  }
+
+  // Color de fondo
+  al_clear_to_color(al_map_rgb(0, 0, 0));
+  al_flip_display();
+
+  const char *device = "enp0s3";
+  char error_buffer[PCAP_ERRBUF_SIZE];
+  int packets_count = 5;
+
+  pcap_t *capdev = pcap_open_live(device, BUFSIZ, 0, -1, error_buffer);
+
+  if (capdev == NULL) {
+    printf("ERR: pcap_open_live() %s\n", error_buffer);
+    al_destroy_display(display);
+    exit(1);
+  }
+
+  int link_hdr_type = pcap_datalink(capdev);
+
+  switch (link_hdr_type) {
+    case DLT_NULL:
+      link_hdr_length = 4;
+      break;
+    case DLT_EN10MB:
+      link_hdr_length = 14;
+      break;
+    default:
+      link_hdr_length = 0;
+  }
+
+  if (pcap_loop(capdev, packets_count, call_me, (u_char *)NULL)) {
+    printf("ERR: pcap_loop() failed!\n");
+    al_destroy_display(display);
+    exit(1);
+  }
+
+  al_destroy_display(display);
+
+  return 0;
 }
